30c2b687aad7114ebf70813740efe90a768cfcae
SebMay99 <tianmayorga2008@gmail.com>
2026-08-10 11:57:40 -0600

    feat: refactored raw data fetch function

--- src/main.py
+++ src/main.py
@@ -2,7 +2,9 @@ from services.ivoa_tap import fetch_raw_data
 
 # Main loop
 if __name__ == "__main__":
+    tap_service_url = "https://exoplanetarchive.ipac.caltech.edu/TAP"
     try:
-        fetch_raw_data()
+        raw_data = fetch_raw_data(tap_service_url)
+        print(raw_data)
     except Exception as e:
         print(f"Error occurred: {e}")
--- src/services/ivoa_tap.py
+++ src/services/ivoa_tap.py
@@ -1,18 +1,15 @@
 # IVOA TAP Service for NASA Exoplanet Archive
 import pyvo as vo
-import pandas as pd
 
-def fetch_raw_data():
+def fetch_raw_data(tap_service_url: str) -> DataFrame:
     # Define the TAP service URL for NASA Exoplanet Archive
-    tap_service_url = "https://exoplanetarchive.ipac.caltech.edu/TAP"
 
     # Create a TAP service object
     tap_service = vo.dal.TAPService(tap_service_url)
 
     # Define the ADQL query to fetch exoplanet data
     adql_query = """
-        SELECT TOP 5
-        pl_name, pl_orbper, pl_rade, pl_bmasse, st_teff, st_rad
+        SELECT pl_name, pl_orbper, pl_rade, pl_bmasse, st_teff, st_rad
         FROM ps
         WHERE pl_orbper IS NOT NULL AND pl_rade IS NOT NULL
         """
@@ -22,6 +19,6 @@ def fetch_raw_data():
         result = tap_service.search(adql_query)
         df = result.to_table().to_pandas()
         print("Fetched data successfully:")
-        print(df)
+        return df
     except Exception as e:
         print(f"Error occurred while fetching data: {e}")
